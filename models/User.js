955ca754cd706ef0914f1e0f4d4295eed2c44913
Carolina Haggerty <work@Carolinas-Mac.local>
2023-05-06 17:19:39 -0700

    added User model with attributes and hook for password bcrypt

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [8],
+        },
+      }
+  },
+  {
+    hooks: {
+        beforeCreate: async (newUser) => {
+          try {
+            newUser.password = await bcrypt.hash(newUser.password, 5);
+            return newUser;
+          } catch (err) {
+            console.log(err);
+            return err;
+          }
+        }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
