ad78dd2d39c68893ca3d423e1ffd7a6ea3b79bc5
Cyndi Fosmark <cmfosmark@gmail.com>
2023-05-09 13:28:21 -0700

    just messing around with handlebars and css

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const hbs = exphbs.create({});
 app.engine ("handlebars", hbs.engine);
 app. set("view engine", "handlebars");
 
+app.use(express.static('./public/images'));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(require("./controllers/exercise-routes"));
 // -----for Handlebars--------
