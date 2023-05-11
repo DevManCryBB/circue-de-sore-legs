77a07bae163d3dc53d697f0b501c090cdf7a44c5
DevManCryBB <veronicagriggs3@gmail.com>
2023-05-11 12:03:55 -0700

    logout functionality

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,5 +1,6 @@
 document.querySelector("#logout").addEventListener("click",e=>{
     e.preventDefault();
+    console.log("hello")
     fetch("/api/users/logout",{
         method:"POST",
     }).then(res=>{
