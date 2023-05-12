395864b9bc18145dcba55fb47e90e1df4e57798d
Cyndi Fosmark <cmfosmark@gmail.com>
2023-05-11 23:29:54 -0700

    wayy more css

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -63,11 +63,23 @@ router.get("/landing", async(req,res)=>{
     const userData = await Users.findOne({ where: { id:req.session.user_id } });
      const allFavorites = await userData.getExercises();
      const hbsData = allFavorites.map(exercise => exercise.get({plain:true}));
-      res.render("landing",{user:userData.name, allCategories:hbsDataCategory , allFavorites:  hbsData})
+      res.render("landing",{user:userData.name, allCategories:hbsDataCategory , allFavorites:  hbsData});
   }catch(err){
     res.status(400).json(err);
   }
-})
+});
+
+router.get("/favorites", async(req,res)=>{
+  try{
+    const userData = await Users.findOne({ where: { id:req.session.user_id } });
+    const allFavorites = await userData.getExercises();
+  const hbsData = allFavorites.map(exercise => exercise.get({plain:true}));
+  console.log("Extracted hbsData: " + hbsData);
+  res.render("favorites",{allFavorites: hbsData});
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 
 
